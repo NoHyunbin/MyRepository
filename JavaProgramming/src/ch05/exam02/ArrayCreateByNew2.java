e81899e4a1184ba0a8415b2606bad3f2d87530da
NoHyunbin <gusqls0810@naver.com>
2016-08-22 09:10:35 +0900

    ArrayCreateByNew1, 2 change

--- JavaProgramming/src/ch05/exam02/ArrayCreateByNew2.java
+++ JavaProgramming/src/ch05/exam02/ArrayCreateByNew2.java
@@ -3,23 +3,25 @@ package ch05.exam02;
 public class ArrayCreateByNew2 {
 
 	public static void main(String[] args) {
-		// 기본 타입 배열
-		int[] arr1 = new int[3];
-		arr1[0] = 10;
-		arr1[1] = 20;
-		arr1[2] = 30;
+		int[] arr1 = new int[5];
 		
 		for ( int i = 0 ; i < arr1.length ; i++ ) {
 			System.out.println("arr1[" + i + "] : " + arr1[i]);
 		}
 		
-		System.out.println(arr1[0] == arr1[1]);
+		double[] arr2 = new double[5];
 		
-		// 참조 타입 배열
-		String[] arr4 = new String[3];
-		arr4[0] = "자바";
-		arr4[1] = "자바";
-		arr4[2] = "대한민국";
+		for ( int i = 0 ; i < arr2.length ; i++ ) {
+			System.out.println("arr2[" + i + "] : " + arr2[i]);
+		}
+		
+		boolean[] arr3 = new boolean[5];
+		
+		for ( int i = 0 ; i < arr3.length ; i++ ) {
+			System.out.println("arr3[" + i + "] : " + arr3[i]);
+		}
+		
+		String[] arr4 = new String[5];
 		
 		for ( int i = 0 ; i < arr4.length ; i++ ) {
 			System.out.println("arr4[" + i + "] : " + arr4[i]);
