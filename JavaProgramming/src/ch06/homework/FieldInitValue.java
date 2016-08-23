8eaf7dfea46e5e562c6fea9f478c2a867276e873
NoHyunbin <gusqls0810@naver.com>
2016-08-23 18:58:07 +0900

    160823 과제 완료

---
+++ JavaProgramming/src/ch06/homework/FieldInitValue.java
@@ -0,0 +1,19 @@
+package ch06.homework;
+
+public class FieldInitValue {
+	// 필드
+	byte byteField = 1;
+	short shortField = 2;
+	int intField = 3;
+	long longField = 4;
+	
+	boolean booleanField = true;
+	char charField = 'S';
+	
+	float floatField = 5;
+	double doubleField = 5;
+	
+	int[] arrField = { 1, 2, 3 };
+	String referenceField = "실습";
+	
+}
---
+++ JavaProgramming/src/ch06/homework/FieldInitValueExample.java
@@ -0,0 +1,24 @@
+package ch06.homework;
+
+public class FieldInitValueExample {
+
+	public static void main(String[] args) {
+		FieldInitValue fiv = new FieldInitValue();
+		
+		System.out.println("byteField : " + fiv.byteField);
+		System.out.println("shortField : " + fiv.shortField);
+		System.out.println("intField : " + fiv.intField);
+		System.out.println("longField : " + fiv.longField);
+		System.out.println("booleanField : " + fiv.booleanField);
+		System.out.println("charField : " + fiv.charField);
+		System.out.println("floatField : " + fiv.floatField);
+		System.out.println("doubleField : " + fiv.doubleField);
+		System.out.println("arrField : " + fiv.arrField);
+		System.out.println("arrField : " + fiv.arrField[0]);
+		System.out.println("arrField : " + fiv.arrField[1]);
+		System.out.println("arrField : " + fiv.arrField[2]);
+		System.out.println("referenceField : " + fiv.referenceField);
+		
+	}
+
+}
