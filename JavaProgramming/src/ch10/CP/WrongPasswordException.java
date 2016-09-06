3dc184a8052dddc836d9aa939f0d2d73ae2ad9c2
NoHyunbin <gusqls0810@naver.com>
2016-09-06 19:10:15 +0900

    2016. 09. 06. Tue 수업끝!!!!

---
+++ JavaProgramming/src/ch10/CP/WrongPasswordException.java
@@ -0,0 +1,10 @@
+package ch10.CP;
+
+public class WrongPasswordException extends Exception{
+
+	public WrongPasswordException() {}
+	public WrongPasswordException(String message) {
+		super(message);
+	}
+	
+}
--- JavaProgramming/src/ch10/exam02/Example.java
+++ JavaProgramming/src/ch10/exam02/Example.java
@@ -4,7 +4,7 @@ public class Example {
 
 	public static void main(String[] args) {
 		try {
-			int result = Calculator.sum("10", "ㅁ");
+			int result = Calculator.sum("10", "a");
 			System.out.println("result : " + result);
 		} catch ( NumberFormatException e ) {
 			System.out.println("숫자가 입력되지 않았네~");
