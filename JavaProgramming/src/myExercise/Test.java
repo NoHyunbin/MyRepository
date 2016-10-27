36dd6d166a6568e2241e6d13259d8f032ec6f125
NoHyunbin <gusqls0810@naver.com>
2016-10-27 18:52:45 +0900

    2016. 10. 27. Thur

--- JavaProgramming/src/myExercise/Test.java
+++ JavaProgramming/src/myExercise/Test.java
@@ -1,38 +1,12 @@
 package myExercise;
 
+import java.util.Scanner;
+
 public class Test {
 
 	public static void main(String[] args) {
-		
-		// array output compare
-		int[] score = {1, 2, 3};
-		System.out.println(score);
-		System.out.println(score[0]);
-		
-		// String output compare
-		String name1 = new String("신용권");
-		String name2 = new String("신용권");
-		String name3 = name2;
-		
-		boolean x;
-		boolean y;
-		boolean z;
-		
-		x = name1 == name2;
-		y = name1.equals(name2);
-		z = name2 == name3;
-		
-		System.out.println(name1);
-		System.out.println(name2);
-		System.out.println(name3);
-		
-		System.out.println(x);
-		System.out.println(y);
-		System.out.println(z);
-		
-		System.out.println(name1.hashCode());
-		System.out.println(name2.hashCode());
-		System.out.println(name3.hashCode());
+		Scanner scan = new Scanner(System.in);
+
 		
 	}
 
--- SpringFinalProgramming/src/main/webapp/WEB-INF/views/home.jsp
+++ SpringFinalProgramming/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,4 +1,5 @@
 <%@ page contentType="text/html; charset=UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
 <!DOCTYPE html>
 <html>
@@ -8,7 +9,16 @@
 	<body>
 		SpringBoardProgramming
 		<hr/>
-		1. <a href="<%=pageContext.getServletContext().getContextPath()%>/member/login">로그인</a> <br/>
+		1. 
+			<c:if test="${login == null}"> 
+				<a href="<%=pageContext.getServletContext().getContextPath()%>/member/login">로그인</a>
+			</c:if>
+			
+			<c:if test="${login != null}"> 
+				<a href="<%=pageContext.getServletContext().getContextPath()%>/member/logout">로그아웃</a>
+			</c:if>
+			<br/>
+
 		2. <a href="${pageContext.servletContext.contextPath}/freeboard/list">자유 게시판</a> <br/>
 		3. <a href="${pageContext.servletContext.contextPath}/photoboard/list">포토 게시판</a> <br/>
 	</body>
