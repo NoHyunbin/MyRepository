e1987cd6298675e5d664434918f781ef0eea834d
NoHyunbin <gusqls0810@naver.com>
2016-11-08 10:34:30 +0900

    수정됨

--- AndroidProgramming/Exam01_Activity_LifeCycle/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ AndroidProgramming/Exam01_Activity_LifeCycle/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.administrator.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity { // 앱만들 때 꼭 필요한 부모 클래스
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); // 재정의 하고도, 부모클래스 내의 메소드의 내용이 필요해서 쓴거
+        setContentView(R.layout.activity_main); // R은 res, layout은 layout, activity_main은 activity_main.xml을 나타낸다
+        Log.i("mylog", "onCreate()");
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("mylog", "onDestroy()");
+    }
 }
