80ddbe2d6e25e187d1d5fbf15af00ee917a0b3a0
Hallu <Hallu@Hallu-PC>
2016-11-10 09:13:48 +0900

    2016 1109 프로젝트

---
+++ Restaurant/src/main/java/com/mycompany/restaurant/dto/CouponBox.java
@@ -0,0 +1,20 @@
+package com.mycompany.restaurant.dto;
+
+public class CouponBox {
+	private String cbMid;
+	private int cbnumber;
+	
+	public String getCbMid() {
+		return cbMid;
+	}
+	public void setCbMid(String cbMid) {
+		this.cbMid = cbMid;
+	}
+	public int getCbnumber() {
+		return cbnumber;
+	}
+	public void setCbnumber(int cbnumber) {
+		this.cbnumber = cbnumber;
+	}
+
+}
---
+++ Restaurant/src/main/java/com/mycompany/restaurant/service/ReservationService.java
@@ -0,0 +1,14 @@
+package com.mycompany.restaurant.service;
+
+public class ReservationService {
+	public int reservation(String rvmid, int rvResid) {
+		
+		return 0;
+	}
+	
+	public int delete(String rvmid, int rvResid) {
+		
+		return 0;
+	}
+	
+}
