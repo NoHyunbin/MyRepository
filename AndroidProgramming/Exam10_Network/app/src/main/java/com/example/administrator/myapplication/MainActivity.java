f7cc8a1e2ffc0ede79ded29ad33e30639b00d0d3
NoHyunbin <gusqls0810@naver.com>
2016-11-22 21:52:03 +0900

    .

--- AndroidProgramming/Exam10_Network/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ AndroidProgramming/Exam10_Network/app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.myapplication;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,31 +48,83 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 final Light light = (Light) lightAdapter.getItem(position);
-                Thread thread = new Thread() {
+
+                AsyncTask<Void, Void, Bitmap> asyncTask = new AsyncTask<Void, Void, Bitmap>() {
                     @Override
-                    public void run() {
-                        final Bitmap bitmap = getBitmap(light.getImageLargeFileName());
-                        imageLarge.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                imageLarge.setImageBitmap(bitmap);
-                            }
-                        });
+                    protected Bitmap doInBackground(Void... params) {
+                        return getBitmap(light.getImageLargeFileName());
+                    }
+
+                    @Override
+                    protected void onPostExecute(Bitmap bitmap) {
+                        imageLarge.setImageBitmap(bitmap);
                     }
                 };
-                thread.start();
+
+                asyncTask.execute();
             }
         });
 
+        testAsyncTask();
         fillItems();
     }
 
 
-    public void fillItems() {
-        // HTTP 통신 코드
-        Thread thread = new Thread() {
+    public void testAsyncTask() {
+        Log.i("mylog", "1 : " + Thread.currentThread().getName());
+        // 어떠한 데이터도 받지 않겠다는 의미로 Void를 사용함
+        // 첫번째 제네릭 파라미터의 타입은 asyncTask.excute();의 매개값의 타입이다. 그리고 그 값은 doInBackground의 파라미터의 타입이다.
+        // 두번째 제네릭 파라미터의 타입은 publishProgress()가 실행될 때의 매개값이다. onProgressUpdate()의 파라미터 타입도 같게 해줘야 한다.
+        // 세번째 제네릭 파라미터의 타입은 doInBackground의 리턴타입이다. onPostExecute()의 파라미터로 리턴값이 전달되므로 파라미터 타입도 같게 해줘야 한다.
+        AsyncTask<String, Integer, String> asyncTask = new AsyncTask<String, Integer, String>() {
             @Override
-            public void run() {
+            protected String doInBackground(String... params) {
+                Log.i("mylog", "2 : " + Thread.currentThread().getName());
+                Log.i("mylog", params[0]);
+                // publishProgress(); // 이걸 메인스레드에 제출하면 onProgressUpdate가 실행됨
+                Log.i("mylog", params[1]);
+                Log.i("mylog", params[2]);
+
+                for (int i = 1; i <= 100; i++) {
+                    if (i == 1) {
+                        publishProgress(1);
+                    } else if (i == 50) {
+                        publishProgress(50);
+                    } else if (i == 100) {
+                        publishProgress(100);
+                    }
+                }
+
+                return "작업스레드결과";
+            }
+
+            // 현재 작업이 얼마나 진행됐는지 알고자 할 때 실행하는 부분
+            // doInBackground에서 실행하라고 요청했을 때에만 실행됨
+            @Override
+            protected void onProgressUpdate(Integer... values) {
+                Log.i("mylog", "3 : " + Thread.currentThread().getName());
+                Log.i("mylog", "작업 진행 정도 : " + values[0]);
+            }
+
+            // 아래 부분은 main thread에서 실행된다.
+            // UI 변경은 main thread에서만 해야하므로 여기서 UI 변경코드를 넣으면 된다.
+            // doInBackground의 리턴값이 파라미터로 온다.
+            @Override
+            protected void onPostExecute(String aVoid) {
+                Log.i("mylog", "4 : " + Thread.currentThread().getName());
+                Log.i("mylog", aVoid);
+            }
+        };
+
+        asyncTask.execute("실행매개값1", "실행매개값2", "실행매개값3");
+    }
+
+
+    public void fillItems() {
+        AsyncTask<Void, Void, List<Light>> asyncTask = new AsyncTask<Void, Void, List<Light>>() {
+            @Override
+            protected List<Light> doInBackground(Void... params) {
+                List<Light> list = null;
                 try {
                     URL url = new URL("http://192.168.0.29:8080/myandroid/lightList");
                     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -87,27 +140,29 @@ public class MainActivity extends AppCompatActivity {
                             if (data == null) break;
                             strJson += data;
                         }
-                        br.close(); reader.close(); is.close();
-                        final List<Light> list = parseJson(strJson);
-                        lightList.post(new Runnable() {
-                            // post메소드가 메인 스레스에서 실행하게끔 요청한다.
-                            // 이렇게 하면 이 run()은 Main Thread가 실행한다.
-                            @Override
-                            public void run() {
-                                lightAdapter = new LightAdapter(MainActivity.this);
-                                lightAdapter.setList(list);
-                                lightList.setAdapter(lightAdapter);
-                                imageLarge.setImageBitmap(list.get(0).getImage());
-                            }
-                        });
+                        br.close();
+                        reader.close();
+                        is.close();
+                        list = parseJson(strJson);
                     }
                     conn.disconnect();
                 } catch (Exception e) {
                     Log.i("mylog", e.getMessage());
                 }
+                return list;
+            }
+
+            @Override
+            protected void onPostExecute(List<Light> lights) {
+                lightAdapter = new LightAdapter(MainActivity.this);
+                lightAdapter.setList(lights);
+                lightList.setAdapter(lightAdapter);
+                imageLarge.setImageBitmap(lights.get(0).getImageLarge());
             }
         };
-        thread.start();
+
+        asyncTask.execute();
+
     }
 
     public List<Light> parseJson(String strJon) {
